117a510e10c36cfb2ae83066d43bf6d5d37a2419
pasibun <matthijs_hovestad@hotmail.com>
2026-02-28 13:24:06 +0100

    feat: integrate PublicCode schema and update repository models to include mandatory fields

--- pkg/oss_client/models/repositorie.go
+++ pkg/oss_client/models/repositorie.go
@@ -13,6 +13,70 @@ import (
 	"time"
 )
 
+type PublicCode struct {
+	PubliccodeYmlVersion string                            `json:"publiccodeYmlVersion,omitempty"`
+	Name                 string                            `json:"name,omitempty"`
+	Url                  string                            `json:"url,omitempty"`
+	Platforms            []string                          `json:"platforms,omitempty"`
+	DevelopmentStatus    string                            `json:"developmentStatus,omitempty"`
+	SoftwareType         string                            `json:"softwareType,omitempty"`
+	Legal                *PublicCodeLegal                  `json:"legal,omitempty"`
+	Description          map[string]PublicCodeDescription  `json:"description,omitempty"`
+	Maintenance          *PublicCodeMaintenance            `json:"maintenance,omitempty"`
+	Localisation         *PublicCodeLocalisation           `json:"localisation,omitempty"`
+	Organisation         *PublicCodeOrganisation           `json:"organisation,omitempty"`
+	DependsOn            *PublicCodeDependsOn              `json:"dependsOn,omitempty"`
+	FundedBy             []PublicCodeOrganisationReference `json:"fundedBy,omitempty"`
+}
+
+type PublicCodeLegal struct {
+	License string `json:"license,omitempty"`
+}
+
+type PublicCodeDescription struct {
+	ShortDescription string   `json:"shortDescription,omitempty"`
+	LongDescription  string   `json:"longDescription,omitempty"`
+	Features         []string `json:"features,omitempty"`
+}
+
+type PublicCodeMaintenance struct {
+	Type        string                 `json:"type,omitempty"`
+	Contractors []PublicCodeContractor `json:"contractors,omitempty"`
+	Contacts    []PublicCodeContact    `json:"contacts,omitempty"`
+}
+
+type PublicCodeContractor struct {
+	Name  string `json:"name,omitempty"`
+	Until string `json:"until,omitempty"`
+}
+
+type PublicCodeContact struct {
+	Name string `json:"name,omitempty"`
+}
+
+type PublicCodeLocalisation struct {
+	LocalisationReady  *bool    `json:"localisationReady,omitempty"`
+	AvailableLanguages []string `json:"availableLanguages,omitempty"`
+}
+
+type PublicCodeOrganisation struct {
+	Uri string `json:"uri,omitempty"`
+}
+
+type PublicCodeDependsOn struct {
+	Open        []PublicCodeDependency `json:"open,omitempty"`
+	Proprietary []PublicCodeDependency `json:"proprietary,omitempty"`
+	Hardware    []PublicCodeDependency `json:"hardware,omitempty"`
+}
+
+type PublicCodeDependency struct {
+	Name string `json:"name,omitempty"`
+}
+
+type PublicCodeOrganisationReference struct {
+	Name string `json:"name,omitempty"`
+}
+
 type RepositorySummary struct {
 	Id               string               `json:"id" gorm:"column:id;primaryKey"`
 	Url              string               `json:"url" gorm:"column:repository_url"`
@@ -21,6 +85,7 @@ type RepositorySummary struct {
 	PublicCodeUrl    string               `json:"publicCodeUrl,omitempty" gorm:"column:public_code_url"`
 	ShortDescription string               `json:"shortDescription,omitempty" gorm:"column:short_description"`
 	Name             string               `json:"name,omitempty" gorm:"column:name"`
+	PublicCode       *PublicCode          `json:"publicCode,omitempty" gorm:"column:public_code_data;serializer:json"`
 	CreatedAt        time.Time            `json:"createdAt" gorm:"column:created_at"`
 	LastCrawledAt    time.Time            `json:"lastCrawledAt" gorm:"column:last_crawled_at"`
 	LastActivityAt   time.Time            `json:"lastActivityAt,omitempty" gorm:"column:last_activity_at"`
@@ -40,6 +105,7 @@ type Repository struct {
 	OrganisationID   *string       `json:"-" gorm:"column:organisation_id"`
 	Url              string        `json:"url" gorm:"column:repository_url"`
 	PublicCodeUrl    string        `json:"publicCodeUrl,omitempty" gorm:"column:public_code_url"`
+	PublicCode       *PublicCode   `json:"publicCode,omitempty" gorm:"column:public_code_data;serializer:json"`
 	CreatedAt        time.Time     `json:"createdAt" gorm:"column:created_at"`
 	LastCrawledAt    time.Time     `json:"lastCrawledAt" gorm:"column:last_crawled_at"`
 	LastActivityAt   time.Time     `json:"lastActivityAt,omitempty" gorm:"column:last_activity_at"`
